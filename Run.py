a60a4daee5c43c6058da1aff13b6191408cfdbba
aeirya <aeirya@gmail.com>
2020-07-11 21:06:15 +0430

    small change, interface for loading vars

--- Run.py
+++ Run.py
@@ -2,16 +2,25 @@
 
 import time
 import os
-
+import cv2
 import pyautogui as pag
+
+def getUser():
+    return "email", "pass"
+
 if __name__ == '__main__':
     print("Running Script...")
+    sample_img = "login.png"
+    email, password = getUser()
+    
     os.startfile("C:\Program Files (x86)\Overwatch\_retail_\Overwatch.exe")
-    while not pag.locateOnScreen("login.png"):
+    
+    while not pag.locateOnScreen():
         print("login.png not found on screen yet..")
         time.sleep(0.1) #wait until the game loads up
-    pag.typewrite("Email") #Email here
+    
+    pag.typewrite(email) #Email here
     pag.press('tab')
     time.sleep(0.1)
-    pag.typewrite("Password") #Password here
+    pag.typewrite(password) #Password here
     pag.press('enter')
