728439b8335e15c8205cd0ea70e9c495f215e17f
aeirya <aeirya@gmail.com>
2020-07-12 00:02:01 +0430

    locate on screen with open cv2

--- Run.py
+++ Run.py
@@ -4,23 +4,70 @@ import time
 import os
 import cv2
 import pyautogui as pag
+import numpy as np
 
 def getUser():
-    return "email", "pass"
+    return email, password
+
+def readImage(path : str):
+    return cv2.imread(path)
+
+def doTheThing(email, password):
+    pag.typewrite(email)
+    pag.press('tab')
+    time.sleep(0.1)
+    pag.typewrite(password)
+    pag.press('enter')
+
+def findImage(screen, image, threshold):
+    res = cv2.matchTemplate(screen, image, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= threshold)
+    return loc
+
+def isEmpty(array):
+    return not list(array[0])
+
+def compress(screen, sample, rate):
+    return resize(screen, rate), resize(sample, rate)
+
+def locateOnScreen(image):
+    screen = getScreen()
+    compression = 0.5
+    screen, sample = compress(screen, sample, compression)
+    threshold = 0.70
+    loc = findImage(screen, image, threshold)  
+    return not isEmpty(loc)
+
+def pilToNumpy(image):
+    return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+def resize(img, scale):
+    width = int(img.shape[1] * scale)
+    height = int(img.shape[0] * scale)
+    dim = (width, height)
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    return resized
+
+def getScreen():
+    screen = pag.screenshot()
+    screen = pilToNumpy(screen)
+    return screen
+
+def showImageTest(image):
+    cv2.imshow('image',image)
+    cv2.waitKey(0)
+
+email = "my mail"
+password = "my password"
+sample_img = "sample.png"
+launcher = "C:\Program Files (x86)\Overwatch\_retail_\Overwatch.exe"
 
 if __name__ == '__main__':
     print("Running Script...")
-    sample_img = "login.png"
     email, password = getUser()
-    
-    os.startfile("C:\Program Files (x86)\Overwatch\_retail_\Overwatch.exe")
-    
-    while not pag.locateOnScreen():
-        print("login.png not found on screen yet..")
-        time.sleep(0.1) #wait until the game loads up
-    
-    pag.typewrite(email) #Email here
-    pag.press('tab')
-    time.sleep(0.1)
-    pag.typewrite(password) #Password here
-    pag.press('enter')
+    # os.startfile(launcher)
+    sample = readImage(sample_img)
+    while not locateOnScreen(sample):
+        print("trying to locate element..")
+        time.sleep(0.1)
+    # doTheThing(email, password)
